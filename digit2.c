073002df4d7fda447395121cd42e1cdc8e1b3958
rockermoney <35295528+rockermoney@users.noreply.github.com>
2018-02-09 15:49:25 +0530

    Create digit2.c

---
+++ digit2.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{int ch;
+printf("Enter the number");
+scanf("%d",&ch); printf("\n%d",ch);
+switch(ch)
+{case 1:
+printf("\none");
+break;
+case 2:
+printf("\nTwo");
+break;
+case 3:
+printf("\nThree");
+break;
+case 4:
+printf("\nFour");
+break;
+case 5:
+printf("\nFive");
+break;
+case 6:
+printf("\nSix");
+break;
+case 7:
+printf("\nSeven");
+break;
+case 8:
+printf("\nEight");
+break;
+case 9:
+printf("\nNine");
+break;
+case 10:
+printf("\nTen");
+break;}
+return 0;}
